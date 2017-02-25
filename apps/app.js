415dc22edaca385f797113ea9dff73c8f7cdf62b
Tiana Tucker <tianatucker@gmail.com>
2017-02-25 14:44:02 -0500

    Add completed functionality

---
+++ apps/app.js
@@ -0,0 +1,39 @@
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+  console.log(getRequest);
+});
+
+function getRequest(searchTerm){
+  var params = {
+    part: 'snippet',
+    key: 'AIzaSyDGykT8bbY3M1peihLLyo_hmZw10UKukns',
+    q: searchTerm
+  };
+  url = 'https://www.googleapis.com/youtube/v3/search';
+
+  $.getJSON(url, params, function(data){
+    console.log(data.items);
+    showResults(data.items);
+  });
+}
+
+/*function getRequest(searchTerm){
+  $.getJSON('https://www.googleapis.com/youtube/v3/search/&part=' + 'snippet' + '&key=AIzaSyDGykT8bbY3M1peihLLyo_hmZw10UKukns?s=' + searchTerm, function(data){
+    showResults(data.Search);
+  });
+}*/
+
+function showResults(results){
+  var html = "";
+  //console(html);
+  $.each(results, function(index,value){
+    html += '<p>' + '<img src="' + value.snippet.thumbnails.medium.url + '" >' + value.snippet.title + '</p>';
+    console.log(value.snippet.title);
+  });
+  //console(value.snippet.title);
+  $('#search-results').html(html);
+}
